aff3207441621c7855d724303435b1b08f5fd62f
rithulraj <rithul97@gmail.com>
2022-05-22 22:09:26 +0530

    MVVM implemented

--- app/src/main/java/com/bridge/global/mainScreen/MainActivity.kt
+++ app/src/main/java/com/bridge/global/mainScreen/MainActivity.kt
@@ -13,7 +13,7 @@ import com.bridge.global.room.UserRepository
 class MainActivity : AppCompatActivity() {
   private var usersRecyclerView: RecyclerView? = null
   private var adapter: UsersListAdapter? = null
-  private lateinit var  viewModel: MainViewModel
+  private lateinit var viewModel: MainViewModel
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
